8ea9193975f156f288b3d5285c1509f2a4289124
Dragan Golic <golicdragan0@gmail.com>
2022-09-28 22:47:33 -0400

    Something

---
+++ main.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous person: ")
+
+madlib = f" Beekeeping is so {adj}! It make me so relaxed because I love to be with the bees and {verb1}. To spend time in \
+the nature and {verb2} like you are {famous_person}!"
+
+print(madlib)
